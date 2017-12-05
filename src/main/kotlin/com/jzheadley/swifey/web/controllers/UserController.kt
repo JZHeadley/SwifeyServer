4518f686bd930dfe39d67a9faa7e1fad743669a5
J. Zephyr Headley <headleyjz@vcu.edu>
2017-12-05 13:24:11 -0500

    swapped the UserController create user endpoint to using the native save method rather than mine as allowed by Cano

--- src/main/kotlin/com/jzheadley/swifey/web/controllers/UserController.kt
+++ src/main/kotlin/com/jzheadley/swifey/web/controllers/UserController.kt
@@ -2,19 +2,28 @@ package com.jzheadley.swifey.web.controllers
 
 import com.jzheadley.swifey.domain.User
 import com.jzheadley.swifey.util.ResponseUtil
+import com.jzheadley.swifey.web.repositories.PhoneRepository
 import com.jzheadley.swifey.web.repositories.UserRepository
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
 @RequestMapping("/api/users")
-class UserController(val userRepository: UserRepository) {
+class UserController(val userRepository: UserRepository, val phoneRepository: PhoneRepository) {
     @GetMapping("/")
     fun getAllUsers(): ResponseEntity<List<User>> = ResponseEntity.ok(userRepository.findAll())
 
     @GetMapping("/{id}")
     fun getUSerById(userId: String): ResponseEntity<User> = ResponseUtil.wrapOrNotFound(Optional.ofNullable(userRepository.findById(userId)))
+
+    @PostMapping("/")
+    fun createUser(@RequestBody user: User): ResponseEntity<User> {
+        // Below is what would be if The saves returned what I needed
+//        val phone = phoneRepository.save(user.phone)
+//        println("The saved phone was:\t" + phone)
+//        userRepository.save(user.userId, user.firstName, user.lastName, user.dob, user.numSwipes, phone)
+        // This saves both at once in one call.  The mostly equivalent sql is in the repositories
+        return ResponseUtil.wrapOrNotFound(Optional.ofNullable(userRepository.save(user)))
+    }
 }
--- src/main/kotlin/com/jzheadley/swifey/web/repositories/PhoneRepository.kt
+++ src/main/kotlin/com/jzheadley/swifey/web/repositories/PhoneRepository.kt
@@ -2,9 +2,14 @@ package com.jzheadley.swifey.web.repositories
 
 import com.jzheadley.swifey.domain.Phone
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @SuppressWarnings("unused")
 @Repository
-interface PhoneRepository : JpaRepository<Phone, Long>
+interface PhoneRepository : JpaRepository<Phone, Long> {
+
+//    @Modifying
+//    @Query(value = "insert into phoneNumbers (countryCode,areaCode,exchangeNum,lineNum)  values(?1,?2,?3,?4)", nativeQuery = true)
+//    fun save(countryCode: Int, areaCode: Int, exchangeNum: Int, lineNum: Int): Phone
+
+}

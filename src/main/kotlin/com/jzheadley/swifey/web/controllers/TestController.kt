91e4d641b1681dea9c48703f1b26ef2c0e23eaab
J. Zephyr Headley <headleyjz@vcu.edu>
2017-12-07 14:51:22 -0500

    testing of the fcm library working and sending notifications to phone.

--- src/main/kotlin/com/jzheadley/swifey/web/controllers/TestController.kt
+++ src/main/kotlin/com/jzheadley/swifey/web/controllers/TestController.kt
@@ -1,22 +1,28 @@
 package com.jzheadley.swifey.web.controllers
 
 import de.bytefish.fcmjava.client.FcmClient
+import de.bytefish.fcmjava.model.options.FcmMessageOptions
+import de.bytefish.fcmjava.requests.notification.NotificationMulticastMessage
+import de.bytefish.fcmjava.requests.notification.NotificationPayload
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.Duration
 
 
 @RestController
 @RequestMapping("/api/test")
 class TestController(private val fcmClient: FcmClient) {
 
-//    @GetMapping("/fcm")
-//    fun sendFcm() {
-//        var messageOptions = FcmMessageOptions.builder()
-//                .setTimeToLive(Duration.ofDays(1))
-//                .build()
-//        val notification: NotificationMulticastMessage = NotificationMulticastMessage(messageOptions, )
-//        fcmClient.send(notification)
-//    }
+    @GetMapping("/fcm")
+    fun sendFcm() {
+        var messageOptions = FcmMessageOptions.builder()
+                .setTimeToLive(Duration.ofDays(1))
+                .build()
+        val notification: NotificationMulticastMessage = NotificationMulticastMessage(messageOptions,
+                listOf("cDjnoMy4Ges:APA91bEJZHnVUiUleFLt-a2a7EGlnDVDDHbDvE8KpWfbg8Qoje0-h_cOAhgFD91omSQVF-z9Zw1af5nzohp6ZkoH3jUiNhbGdDBxkotlbWeNPASr26x3MxRVabz3KN_GbEcN-tvl-py4"),
+                NotificationPayload.builder().setBody("Test MEssage sent from the server").setTitle("Testing The Server Sending").build())
+        fcmClient.send(notification)
 
-
+    }
 }
